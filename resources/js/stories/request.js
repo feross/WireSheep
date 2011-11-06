267f7fff30290dde8a6aacb6d7f759bad43e6e57
Feross Aboukhadijeh <feross@gmail.com>
2011-11-05 18:11:26 -0700

    Linking up the models to the UI

--- resources/js/stories/request.js
+++ resources/js/stories/request.js
@@ -2,17 +2,25 @@
 //Returns the url the user was trying to reach for requests
 
 var requestStory = {
-	
+
 	//Applies to requests
 	appliesToPacket: function(packet){
 		return packet['type'] == "request";
 	},
-	
+
 	//Just return the url and time
 	renderStory: function(packet){
 		var url = packet['hostname'] + packet['path'];
 		var now = new Date();
 		var timeStr = now.getHours() + ":" + now.getMinutes();
-		return "Clicked to open:" + url + " at " + timeStr;
+
+		var desc = "Visited <b>Greek prime minister set to form new SDJKHJKLSDHKJLFH - CNN.com</b>.";
+		return {
+			href: url,
+			favicon: "img/favicon.gif",
+			desc: desc,
+			time: timeStr
+		}
 	}
+
 }
