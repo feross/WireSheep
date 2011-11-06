23de6d00c65c1304f4db7ecdbb1de1612ccdd568
John Hiesey <jhiesey@cs.stanford.edu>
2011-11-05 18:07:13 -0700

    Integrated build still br0rked...

--- fireflock.cpp
+++ fireflock.cpp
@@ -3,6 +3,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkDiskCache>
 #include "fireflock.h"
+#include "backendthread.hpp"
 
 Fireflock::Fireflock(QNetworkDiskCache* netcache, QObject* parent)
     : QObject(parent), m_cache(netcache)
@@ -11,6 +12,9 @@ Fireflock::Fireflock(QNetworkDiskCache* netcache, QObject* parent)
         in case it has some images we want: */
     m_network = new QNetworkAccessManager(this);
     m_network->setCache(m_cache);
+    
+    m_backend = new FlockBackend();
+    m_backend->start();
 }
 
 Fireflock::~Fireflock()
